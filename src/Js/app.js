4200b8696b28be259acdd151c124cdfbfd882fdd
Felipe <felipegadelha062@gmail.com>
2024-11-25 14:29:11 -0500

    Drop Down

--- src/Js/app.js
+++ src/Js/app.js
@@ -1,3 +1,17 @@
-function toggleMenu(){
-    document.querySelector('.navbar').classList.toggle('active');
-}
+// function toggleMenu(){
+//     document.querySelector('.navbar').classList.toggle('active');
+// }
+
+  document.querySelector('.dropbtn').addEventListener('click', function(event) {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+    event.stopPropagation(); // Impede que o clique feche o menu imediatamente
+  });
+
+  // Fecha o dropdown se o usuário clicar fora dele
+  document.addEventListener('click', function(event) {
+    var dropdownContent = document.querySelector('.dropdown-content');
+    if (!event.target.closest('.dropdown')) {
+      dropdownContent.style.display = 'none';
+    }
+  });
