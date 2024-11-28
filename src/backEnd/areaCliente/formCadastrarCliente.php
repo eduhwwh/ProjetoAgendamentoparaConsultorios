06de995b2d22520f38f0f62f2da038464ca7e566
Felipe Gadelha <felipegadelha062@gmail.com>
2024-11-28 13:02:13 -0300

    Cadastro de Paciente

--- src/backEnd/areaCliente/formCadastrarCliente.php
+++ src/backEnd/areaCliente/formCadastrarCliente.php
@@ -2,8 +2,16 @@
 
     $nome = $_POST['nome'];
     $sobrenome = $_POST['sobrenome'];
+    $cpf = $_POST['cpf'];
     $email = $_POST['email'];
     $senha = $_POST['senha'];
+    $confirSenha = $_POST['confirSenha'];
+
+    if ($senha !== $confirSenha) {
+        echo "<script>alert('As senhas não são iguais!'); window.history.back();</script>";
+        exit();
+    }
+
 
     try{
         $conn = new PDO("mysql:host=localhost;dbname=clinica", "root", "");
@@ -12,12 +20,18 @@
 
         $hash  = password_hash($senha, PASSWORD_BCRYPT);
 
-        $sql = "INSERT INTO cadcliente(nome,sobrenome,especialidade, crm, email, senha) 
-                    VALUES ('$nome','$sobrenome','$email', '$hash')";
+        $sql = "INSERT INTO cadcliente(nome,sobrenome,cpf,email,senha) 
+                    VALUES (?, ?, ?, ?, ?)";
 
-        $conn->exec($sql);      
+        $stmt = $conn->prepare($sql); 
+        $stmt->bindParam(1, $nome);
+        $stmt->bindParam(2, $sobrenome);
+        $stmt->bindParam(3, $cpf);
+        $stmt->bindParam(4, $email);
+        $stmt->bindParam(5, $hash);
+        $stmt->execute();     
         
-        header('Location:/ProjetoAgendamentoparaConsultorios/html/index.html');
+        header('Location:/ProjetoAgendamentoparaConsultorios/html/loginPaciente.html');
         exit();
 
     }catch(Exception $erro){
